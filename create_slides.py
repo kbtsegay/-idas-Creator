a4d994e54530def432a85242f14760500cb280e3
Kaleb Tsegay <126627596+kbtsegay@users.noreply.github.com>
2024-09-12 17:49:30 -0700

    Update create_slides.py

--- create_slides.py
+++ create_slides.py
@@ -2,5 +2,5 @@ from src.kidase_creator import KidaseCreator
 
 # Example usage with Geez, Tigrinya, and English languages
 if __name__ == '__main__':
-    kidase_creator = KidaseCreator('/mnt/c/Users/samg/Documents/Gits/KidaseCreator/data', ['ግእዝ', 'ትግርኛ', 'english'])
+    kidase_creator = KidaseCreator('./data', ['ግእዝ', 'ትግርኛ', 'english'])
     kidase_creator.create_presentation()
